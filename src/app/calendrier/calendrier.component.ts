9212b69ece4dbc42a7d129a45544f70d5a2b817e
AtyZeph <bennetot-clm@hotmail.com>
2023-07-21 13:45:28 +0200

    Amélioration calendrier (jours mieux alignés)

--- src/app/calendrier/calendrier.component.ts
+++ src/app/calendrier/calendrier.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import * as moment from 'moment';
-import { Jour } from './../../../jour';
 
 @Component({
   selector: 'app-calendrier',
@@ -10,47 +9,58 @@ import { Jour } from './../../../jour';
 export class CalendrierComponent {
   mois: moment.Moment;
   semaines: moment.Moment[][] = [];
-
+  joursSemaineLabels: string[] = [];
 
   constructor() {
     this.mois = moment();
-    this.genererSemaines();
+    this.joursSemaineLabels = this.getJoursSemaineLabels();
+    this.genererCalendrier();
   }
 
-  genererSemaines(): void {
+  getJoursSemaineLabels(): string[] {
+    // Utilisez un tableau pour définir l'ordre souhaité des jours de la semaine
+    const ordreJoursSemaine = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+    const joursLabels = moment.weekdaysMin(); // Utilise les libellés courts des jours de la semaine (Lun, Mar, Mer, ...)
+    const premierJourSemaineIndex = moment().startOf('week').day(); // Index du premier jour de la semaine (0 pour Dimanche, 1 pour Lundi, ...)
+    const joursAvantPremierJourSemaine = joursLabels.splice(0, premierJourSemaineIndex); // Retirer les jours avant le premier jour de la semaine
+    return ordreJoursSemaine;
+  }
+
+  genererCalendrier(): void {
     this.semaines = [];
   
-    const debutMois = this.mois.clone().startOf('month').startOf('week');
-    const finMois = this.mois.clone().endOf('month').endOf('week');
-  
+    const premierJourMois = this.mois.clone().startOf('month');
+    const dernierJourMois = this.mois.clone().endOf('month');
+
     let semaineCourante = [];
-    const dateCourante = debutMois.clone();
+    let dateCourante = premierJourMois.clone().startOf('week').add(1, 'day'); // Commencer par le lundi
   
-    while (dateCourante.isSameOrBefore(finMois, 'day')) {
+    while (dateCourante.isBefore(dernierJourMois, 'day') || dateCourante.isSame(dernierJourMois, 'day')) {
       semaineCourante.push(dateCourante.clone());
   
-      if (dateCourante.day() === 0 && semaineCourante.length > 0) { // Dimanche
+      if (dateCourante.day() === 0) { // Dimanche
         this.semaines.push(semaineCourante);
         semaineCourante = [];
       }
   
       dateCourante.add(1, 'day');
     }
-  
+
+    // Ajouter la dernière semaine
     if (semaineCourante.length > 0) {
       this.semaines.push(semaineCourante);
     }
   }
-  
-  
 
   moisPrecedent(): void {
     this.mois.subtract(1, 'month');
-    this.genererSemaines();
+    this.joursSemaineLabels = this.getJoursSemaineLabels();
+    this.genererCalendrier();
   }
 
   moisSuivant(): void {
     this.mois.add(1, 'month');
-    this.genererSemaines();
+    this.joursSemaineLabels = this.getJoursSemaineLabels();
+    this.genererCalendrier();
   }
 }
