fbf31aa29a07113ad83c9236dbabda5a540b0ddb
Niateph <niateph@gmail.com>
2023-07-28 12:01:44 +0200

    mise en forme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projetfinal';
+
+  currentComponent : string = ""
+  constructor(private router : Router){
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+          this.currentComponent = this.getComponentTitle((<NavigationEnd>event).url);
+      }
+    });
+  }
+
+  getComponentTitle(url : string) : string{
+    switch(url){
+      case("/calendar") : return "Calendrier";
+      default : return "Connexion";
+    }
+  }
 }
